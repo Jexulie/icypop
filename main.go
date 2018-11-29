f3b2d533f9fbf89d27bfeeabc8c909d60e291880
Jexulie <fejitj3n@yahoo.com>
2018-11-29 17:02:21 +0300

    first step

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ package main
 // turn into a api maybe
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 type Icypop struct {
@@ -14,21 +16,41 @@ type Icypop struct {
 	Body string
 }
 
-func (s *Icypop) getBody() {
-	resp, err := http.Get(s.URI)
+func (i *Icypop) getBody() {
+	resp, err := http.Get(i.URI)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	defer resp.Body.Close()
 	bodyBytes, errByte := ioutil.ReadAll(resp.Body)
 	if errByte != nil {
 		log.Fatalln(errByte)
 	}
-	s.Body = string(bodyBytes)
+	i.Body = string(bodyBytes)
+}
 
+func (i *Icypop) search(searched string) []string {
+	// seperate search for h1..h6 | a | div etc...
+	r, err := regexp.Compile("^a[\\.\\#]?.*?$")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	found := r.MatchString(searched)
+	if found {
+		return searchLink(i.Body)
+	}
+	return nil
+}
+
+func searchLink(s string) []string {
+	r, _ := regexp.Compile("<a.*>(.*)</a>")
+	found := r.FindAllString(s, -1)
+	return found
 }
 
 func main() {
 	first := Icypop{URI: "http://tureng.com/en/turkish-english"}
 	first.getBody()
-
+	list := first.search("a.a")
+	fmt.Println(list)
 }
